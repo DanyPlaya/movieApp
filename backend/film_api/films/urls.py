bf68712423500898b81cd0499f1af6ecd7168630
kotov anton <fynjybq_njyz5@mail.ru>
2024-06-19 17:46:47 +0300

    venv

--- backend/film_api/films/urls.py
+++ backend/film_api/films/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('directors/', views.directors_list, name='directors-list'),
     path('genres/', views.genres_list, name='genres-list'),
+    path('films/<int:film_id>/actors/', views.actors_by_film, name='actors-by-film'),
+    path('actors/<int:actor_id>/films/', views.films_by_actor, name='films-by-actor'),
 ]
--- backend/film_api/films/views.py
+++ backend/film_api/films/views.py
@@ -1,10 +1,13 @@
 # films/views.py
 
 from rest_framework import viewsets
-from .models import Film, Actor
+from .models import Film, Actor, Role
 from .serializers import FilmSerializer, ActorSerializer
 from django.http import JsonResponse
 from django.db.models import Count
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 class FilmViewSet(viewsets.ModelViewSet):
     queryset = Film.objects.all()
@@ -19,10 +22,9 @@ class FilmViewSet(viewsets.ModelViewSet):
         end_year = self.request.query_params.get('end_year')
         director = self.request.query_params.get('director')
         studio = self.request.query_params.get('studio')
-
         # Применяем фильтры
         if genre:
-            queryset = queryset.filter(genre__icontains=genre)
+            queryset = queryset.filter(genre=genre)
         
         if start_year and end_year:
             queryset = queryset.filter(release_year__gte=start_year, release_year__lte=end_year)
@@ -39,6 +41,26 @@ class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
+@api_view(['GET'])
+def actors_by_film(request, film_id):
+    try:
+        roles = Role.objects.filter(film_id=film_id)
+        actors = [role.actor for role in roles]
+        serializer = ActorSerializer(actors, many=True)
+        return Response(serializer.data)
+    except Role.DoesNotExist:
+        return Response({"message": "Фильм не найден"}, status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['GET'])
+def films_by_actor(request, actor_id):
+    try:
+        roles = Role.objects.filter(actor_id=actor_id)
+        films = [role.film for role in roles]
+        serializer = FilmSerializer(films, many=True)
+        return Response(serializer.data)
+    except Role.DoesNotExist:
+        return Response({"message": "Актер не найден"}, status=status.HTTP_404_NOT_FOUND)
+    
 
 def directors_list(request):
     directors = Film.objects.values('director').annotate(count=Count('id'))
